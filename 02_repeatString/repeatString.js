0867f43f5f718da18aaa05e81ed49468ca6bb83b
Mohamed Sofy <mediumsofy@gmail.com>
2024-04-21 09:34:13 +0200

    Add 02_repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, n) {
+  let repeatedString = ''
+  if(n < 0) return 'ERROR'
+  for (let i = 0; i < n; i++) 
+  {
+    repeatedString += string
+  }
+  return repeatedString
 };
 
 // Do not edit below this line
