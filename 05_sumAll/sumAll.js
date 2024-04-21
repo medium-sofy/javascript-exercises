5b791ae7594f56cb86e4d0df484b4b3128bcee0d
Mohamed Sofy <mediumsofy@gmail.com>
2024-04-21 13:40:47 +0200

    Add 05_sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if(start < 0 || end < 0) return 'ERROR'
+  if(typeof start != 'number' || typeof end != 'number') return 'ERROR'
+  if(start > end) {
+    let temp = start
+    start = end
+    end = temp
+  }
+
+  return end * (end + 1) / 2
 
 };
 
