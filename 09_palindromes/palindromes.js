71366adb6a1eb547f44c15daced8c4a6a94b214e
Mohamed Sofy <mediumsofy@gmail.com>
2024-07-18 10:54:56 +0300

    Add 09-palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // convert to lower case to work with upper-case letters
+  // remove spaces to make a single string
+  // use a method to remove puctuation
+  // use a stack to flip the string and compare
+  // with the cleaned version
 
+ return str.replace(/[^\w\s]/g, '')
+   .toLowerCase()
+   .split('')
+   .filter((ch)=> ch!==' ')
+   .reverse()
+   .join('') === str.replace(/[^\w\s]/g, '')
+                .toLowerCase()
+                .split('')
+                .filter((item)=> item!==' ')
+                .join('')
 };
 
 // Do not edit below this line
